d6080817dac3eb1f8d532c046433fc4d2cf35823
WMatheus2022 <pachwander4@gmail.com>
2023-04-13 02:38:21 -0300

    Changes to be committed: 	new file:   tipos-variaveis/.vscode/settings.json 	new file:   tipos-variaveis/README.md 	new file:   tipos-variaveis/src/TiposVariaveis.java
     Changes not staged for commit:
    	deleted:    .gitignore
    	deleted:    README.md
    	modified:   tipos-variaveis/README.md
    
     Untracked files:
    	tipos-variaveis/.gitignore

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+    }
+}
